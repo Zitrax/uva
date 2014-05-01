ffaf140f2f23434d96783d2800dd2d161ca51f52
Daniel Bengtsson <daniel@bengtssons.info>
2014-05-01 17:48:40 +0200

    Working 100

---
+++ 100-199/100.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+int cycleLength(int n) {
+  int i=0;
+  while(n!=1) {
+    if(n%2==1) {
+      n = 3*n+1;
+    } else {
+      n /= 2;
+    }
+    ++i;
+  }
+  return i+1;
+}
+
+int maxLength(int i, int j) {
+  int max = 0;
+  for (int x=i;x<=j;++x) {
+    int m = cycleLength(x);
+    if(m>max) {
+      max = m;
+    }
+  }
+  return max;
+}
+
+void output(int i, int j) {
+  cout << i << " " << j << " " << maxLength(i,j) << endl;
+}
+
+int main() {
+  int i,j;
+  while(cin >> i) {
+    cin >> j;
+    output(i, j);
+  } 
+}
