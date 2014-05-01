9b2735aa9e0d76b44393267e9b3f8d1e87b0c4c5
Daniel Bengtsson <daniel@bengtssons.info>
2014-05-01 22:57:19 +0200

    Added caching

--- 100-199/100.cpp
+++ 100-199/100.cpp
@@ -1,25 +1,40 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
+const int MAX_CACHE = 1000000;
+
 int cycleLength(unsigned int n) {
-  int i=0;
+
+  static int cache[MAX_CACHE] = {};
+  if(cache[n] > 0) {
+    return cache[n];
+  }
+
+  vector<unsigned int> path;
+  path.push_back(n);
+
   while(n!=1) {
     if(n%2==1) {
       n = 3*n+1;
     } else {
       n /= 2;
     }
-    ++i;
+    path.push_back(n);
   }
-  return i+1;
+
+  for(unsigned int i=0; i<path.size(); ++i) {
+    if(path[i] < MAX_CACHE)
+      cache[path[i]] = i+1;
+  }
+
+  return path.size();
 }
 
 int maxLength(int i, int j) {
   if( i > j ) {
-    int tmp = i;
-    i = j;
-    j = tmp;
+    swap(i,j);
   }
   int max = 0;
   for(int x=i; x<=j; ++x) {
@@ -40,5 +55,5 @@ int main() {
   while(cin >> i) {
     cin >> j;
     output(i, j);
-  } 
+  }
 }
